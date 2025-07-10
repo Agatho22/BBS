2b71164260e8b78c903566f70b71a157cc2a5bc8
Byeong Hwa Kim <80560485+Agatho22@users.noreply.github.com>
2025-07-10 10:52:54 +0900

    Update FindIDActionServlet.java
    serialVersionUID 선언 경고 제거 // 25.07.10

--- src/main/java/user/FindIDActionServlet.java
+++ src/main/java/user/FindIDActionServlet.java
@@ -10,6 +10,7 @@ import java.io.PrintWriter;
 
 @WebServlet("/findIDAction")
 public class FindIDActionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
